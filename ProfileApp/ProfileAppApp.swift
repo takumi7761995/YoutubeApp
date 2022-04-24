4a19e329b7e301cc54bbe8683f7e53ab05961823
杉本匠 <takumi1995776@gmail.com>
2022-04-24 11:21:00 +0900

    Initial Commit

---
+++ ProfileApp/ProfileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAppApp.swift
+//  ProfileApp
+//
+//  Created by 杉本匠 on 2022/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ProfileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
